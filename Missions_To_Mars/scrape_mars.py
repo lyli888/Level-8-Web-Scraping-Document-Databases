fd58dd0def1526f3317928b967fc883827c20eeb
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-26 20:35:51 -0400

    start scrape_mars.py script

---
+++ Missions_To_Mars/scrape_mars.py
@@ -0,0 +1,9 @@
+from splinter import Browser
+from bs4 import BeautifulSoup as bs
+import time
+import requests
+from webdriver_manager.chrome import ChromeDriverManager
+
+def scrape():
+    
+    return mars_data
