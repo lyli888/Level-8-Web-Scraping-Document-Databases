40451f4eba4d4930f162f42706b65631bd8c6b7c
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-27 08:06:36 -0400

    Update scrape_mars.py

--- Missions_To_Mars/scrape_mars.py
+++ Missions_To_Mars/scrape_mars.py
@@ -2,6 +2,8 @@ from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 import requests
+from flask import Flask, jsonify
+import pymongo 
 from webdriver_manager.chrome import ChromeDriverManager
 
 def scrape():
