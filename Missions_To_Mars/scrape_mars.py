17e0d9f9a650708308cd8fe8fe350bdfbb88604c
Ly Li <76023384+lyli888@users.noreply.github.com>
2021-03-28 00:12:21 -0400

    Update scrape_mars.py

--- Missions_To_Mars/scrape_mars.py
+++ Missions_To_Mars/scrape_mars.py
@@ -146,22 +146,20 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/Mars_db")
 @app.route('/')
 def index():
    # Find data
-    mars_data = mongo.db.mars_data.find_one()
+    mars_info = mongo.db.mars_info.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_data=mars_data)
+    return render_template("index.html", mars_info=mars_info)
 
 @app.route('/scrape')
 def scrape():   
     
-    mars_data = mongo.db.mars_data
     #Scrape New Data
-    mars_data_new= scrape_mars.scrape()
+    mars_data= scrape_mars.scrape()
     #Update with new scraped data
-    mars_data.update({}, mars_data_new, upsert=True)
 
     # Update the Mongo database using update and upsert=True
-    mongo.db.mars_collection.update({}, mars_info, upsert=True)
+    mongo.db.mars_info.update({}, mars_data, upsert=True)
 
     # Redirect back to home page
     return redirect("/")
